bbda3d074b2136089da92e395fdeb2097e9e334f
lulo <lopez.gallego2611@gmail.com>
2025-03-12 23:52:57 -0500

    TODO: time travel

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,29 @@ function Square({value, onSquareClick}) {
   )
 }
 
-export default function Board() {
+export default function Game(){
+  const [xIsNext, setXIsNext] = useState(true)
+  const [history, setHistory] = useState([Array(9).fill(null)])
+  const currentSquares = history[history.length - 1]
+
+  function handlePlay(nextSquares){
+    /*TODO*/
+  }
+  return(
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
+      </div>
+      <div className="game-info">
+        <ol>
+          {/*TODO*/}
+        </ol>
+      </div>
+    </div>
+  )
+}
+
+function Board() {
   const [xIsNext, setXIsNext] = useState(true)
   const [squares, setSquares] = useState(Array(9).fill(null))
 
@@ -70,7 +92,7 @@ function calculateWinner(squares){
   ]
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i]
-    if (squares[a] == squares[a] && squares[b] == squares[a] && squares[c]){
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
       return squares[a]
     }
   }
